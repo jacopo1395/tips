e24367dc2a09f1837e9a292d555be2e4c0ad7be2
SimoneConia <simoneconia@gmail.com>
2016-08-21 19:27:21 +0100

    Add navigation bar and navigation drawer to the header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
 
   def arcsoft
     render html: "Ciao Jacopo, Mattia e Stefan!"
   end
-  
-  
+
+
   protected
 
   def configure_permitted_parameters
