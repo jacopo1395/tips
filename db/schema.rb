944eff4ea867a0e5a8c2f9035f3d3c35cce38e42
SimoneConia <simoneconia@gmail.com>
2016-08-30 17:12:42 +0100

    Add link to admin section

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 20160830082245) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",               default: "",    null: false
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
@@ -58,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160830082245) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.string   "username"
     t.boolean  "admin",                  default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
