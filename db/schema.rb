62376fc539ac0e24a000b9d23b55950cbd121f90
SimoneConia <simoneconia@gmail.com>
2016-09-01 15:02:00 +0100

    Update styling for Facebook login

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 20160830124354) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",               default: "",    null: false
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
@@ -58,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160830124354) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.string   "username"
     t.boolean  "admin",                  default: false
     t.string   "provider"
     t.string   "uid"
