843f48121da2b0feb918ab0a04ae7a212523fc3b
SimoneConia <simoneconia@gmail.com>
2016-09-03 16:12:58 +0100

    Initial commit (studying the apis)

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,17 @@
 class SearchesController < ApplicationController
+
+  def new
+    @client = GooglePlaces::Client.new("AIzaSyBHJpb9fD5eBeN-wd0Xq0vYkTUtRSEgr0U")
+    # spot_list = @client.spots(41.89015, 12.49244)
+    spot_list = @client.spots(41.89015, 12.49244, :types => ['restaurant','food'], :radius => 1000)
+
+    spot_names = Array.new
+    spot_list.each do |spot|
+      spot_names.push spot.name
+      spot_names.push spot.types
+    end
+
+    render html: ("Hello, Google Places!<br><br> #{spot_names}").html_safe
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,22 +7,19 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
 
   get '/profile', to: 'static_pages#profile'
-  
+
   devise_scope :user do get "/sign_in/retry" => "users/sessions#retry" end
   devise_scope :user do get "/sign_up/retry" => "users/registrations#retry" end
-  
-  
+
+
   #devise_scope :user do  delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session end
-  
+
   get '/new_admin', to: 'admins#new_admin'
 
   post '/create_admin', to: 'admins#create_admin'
-  
+
   get '/admin', to: 'admins#admin'  #da cancellare!! Ti fa diventare admin, è solo per test.
-  
-  
-  
-  
-  
+
+  get '/search', to: 'searches#new'
 
 end
