f4b53ad3df91f44455a0a19a50b0f9744c3ab351
SimoneConia <simoneconia@gmail.com>
2016-09-05 15:55:32 +0100

    Initial question styling

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,17 +3,6 @@ class SearchesController < ApplicationController
   include SearchesHelper
 
   def search
-
-    # @search = SearchResults.new(request.location.latitude, request.location.longitude)
-    #
-    # to_render = ""
-    # to_render += @search.place_types_by_number.to_s
-    # to_render += "<br><br><br>"
-    # to_render += @search.place_top_types.to_s
-    # to_render += "<br><br><br>"
-    # to_render += @search.places_by_type.to_s
-    #
-    # render html: to_render.html_safe
   end
 
 end
