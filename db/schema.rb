e8b6784d183694e2c6f7b22de7ca9b2d16d8ec8d
DiFulvio Mattia <mattia.difulvio@gmail.com>
2016-09-09 18:25:34 +0200

    aggiunti campo cap e volte preferito a pois

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909130002) do
+ActiveRecord::Schema.define(version: 20160909161004) do
 
   create_table "apis", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -53,11 +53,13 @@ ActiveRecord::Schema.define(version: 20160909130002) do
     t.string   "image"
     t.text     "review"
     t.string   "api"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.string   "apiId"
     t.float    "lat"
     t.float    "long"
+    t.integer  "cap"
+    t.integer  "voltePreferito"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -89,6 +91,7 @@ ActiveRecord::Schema.define(version: 20160909130002) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "username",               default: "",    null: false
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
@@ -101,7 +104,6 @@ ActiveRecord::Schema.define(version: 20160909130002) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
-    t.string   "username"
     t.boolean  "admin",                  default: false
     t.string   "provider"
     t.string   "uid"
