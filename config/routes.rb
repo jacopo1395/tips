5d41fc3634cb3bea42c9ab53c49d6f447fdb6b7f
SimoneConia <simoneconia@gmail.com>
2016-09-09 14:29:23 +0100

    Reorganize routes.rb and add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,34 @@
 Rails.application.routes.draw do
 
+  # Resources
   resources :pois
-
   resources :searches
 
-  get '/nuovo', to: 'pois#nuovo'
-
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
+  # Home page
   root 'static_pages#home'
 
+  # Home page. "/home" is equal to "/" in this case
   get '/home', to: 'static_pages#home'
 
+  # Path to the user profile. Maybe should be updated to point at the profile page
+  # created by Devise for users?
   get '/profile', to: 'static_pages#profile'
 
-  devise_scope :user do get "/sign_in/retry" => "users/sessions#retry" end
-  devise_scope :user do get "/sign_up/retry" => "users/registrations#retry" end
-
-
-  #devise_scope :user do  delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session end
-
-  get '/new_admin', to: 'admins#new_admin'
-
-  post '/create_admin', to: 'admins#create_admin'
-
-  get '/admin', to: 'admins#admin'  #da cancellare!! Ti fa diventare admin, è solo per test.
-
+  # Start a new search
   get '/search', to: 'searches#search'
 
+  # Admin-related pages
+  get '/new_admin', to: 'admins#new_admin'
+  post '/create_admin', to: 'admins#create_admin'
+
+  # Points of interest related pages
+  get '/nuovo', to: 'pois#nuovo'
+
+  # Devise stuff
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do get "/sign_in/retry" => "users/sessions#retry" end
+  devise_scope :user do get "/sign_up/retry" => "users/registrations#retry" end
+  # devise_scope :user do  delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session end
   devise_scope :user do get "/recent_pois" => "users/recent_pois#getpois" end
 
 end
