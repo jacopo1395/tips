d9fc4f1c725e6d648dabb54ddfa0ad095710c1da
SimoneConia <simoneconia@gmail.com>
2016-09-09 13:50:10 +0100

    Scaffolds.scss messed up all the app layout!

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,5 @@
 class SearchesController < ApplicationController
 
-<<<<<<< HEAD
   include SearchesHelper
 
   before_filter :load_search
@@ -30,17 +29,4 @@ class SearchesController < ApplicationController
     def save_search
       session[:search_object] = @search
     end
-
-=======
-	def index	
-	
-	
-	#  @p=Api.new(request.remote_ip)
-	@p=Api.new("80.183.119.115")
-		#@var=@p.nearby()
-		@var=@p.nearby({:name => "vegetariano", :type => "restaurant"})
-		#@var=@p.text_search("ristoranti roma vegani")
-		render plain: @var
-	 end
->>>>>>> dev
 end
