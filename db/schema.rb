0b4df4c72a950e0b58cb56519fb09ae642682d73
xKedar <mattia.difulvio@gmail.com>
2016-09-26 17:55:55 +0200

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160926150100) do
-
 
   create_table "apis", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -98,6 +96,7 @@ ActiveRecord::Schema.define(version: 20160926150100) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "username",               default: "",    null: false
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
@@ -110,7 +109,6 @@ ActiveRecord::Schema.define(version: 20160926150100) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
-    t.string   "username"
     t.boolean  "admin",                  default: false
     t.string   "provider"
     t.string   "uid"
